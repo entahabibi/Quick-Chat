a702b33d17f6282429d137d93230fcf0374346bc
Zaid Ibrahim <zaidzaheer0321@gmail.com>
2025-09-10 01:48:48 +0500

    Updated

--- server/server.js
+++ server/server.js
@@ -11,40 +11,44 @@ const app = express();
 const server = http.createServer(app)
 
 // intialize socket.io server
-export const io= new Server(server, {
-    cors: {origin: "*"}
+export const io = new Server(server, {
+    cors: { origin: "*" }
 })
 
 // Store online users
 export const userSocketMap = {}; // { user:Id , socket:Id }
 
 // Socket.io connection handler
-io.on("connection", (socket)=>{
+io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
     console.log("User connected", userId);
 
-    if(userId) userSocketMap[userId]= socket.id;
+    if (userId) userSocketMap[userId] = socket.id;
 
     // Emit online users to all connected clients 
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on("disconnect", ()=>{
+    socket.on("disconnect", () => {
         console.log("User Disconnected", userId);
         delete userSocketMap[userId];
         io.emit("getOnlineUsers", Object.keys(userSocketMap))
     })
 })
- // Middleware setup
-app.use(express.json({limit: "4mb"}));
+// Middleware setup
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 //Route setup
-app.use("/api/status", (req, res)=> res.send("Server is live"));
-app.use("/api/auth" , userRouter);  
-app.use("/api/messages" , messageRouter);
+app.use("/api/status", (req, res) => res.send("Server is live"));
+app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
 //Function to connect MongoDB
 await connectDB();
 
-const PORT = process.env.PORT || 5000; 
-server.listen(PORT, ()=> console.log("Server is running on PORT http://localhost:" + PORT))
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => console.log("Server is running on PORT http://localhost:" + PORT))
+}
+// export server for vercel
+export default server;
